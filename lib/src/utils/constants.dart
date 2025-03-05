a1ee9ece2117be0743294e3617e18a9ed615671e
heru oktafian <heru.oktafian@yahoo.com>
2025-03-06 03:16:01 +0700

    Initialize Projects

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ViColors {
   static const Color backGround = Color.fromARGB(255, 245, 245, 245);
@@ -7,4 +8,16 @@ class ViColors {
   static const Color textDefault = Color.fromARGB(255, 85, 122, 83);
   static const Color greyColor = Color(0xffAAAAAA);
   static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color hoverMenu = Color.fromARGB(253, 200, 230, 201);
 }
+
+class ViSize {
+  static const double defaultPadding = 8.5;
+  static const double defaultMargin = 16;
+  static const double defaultRadius = 16;
+  static const double defaultBorder = 1;
+  static const double defaultBorderRadius = 8;
+  static const double defaultFontSize = 12.5;
+}
+
+TextStyle ViDefaultFont = GoogleFonts.raleway();
